a64f7d069a8dd95dac9fd8d8c836980610b00ebf
Cole Beck <clb0119@outlook.com>
2020-07-20 16:03:47 -0500

    modify client, server and testfile

--- projects/project_1/UDPClient.java
+++ projects/project_1/UDPClient.java
@@ -13,56 +13,66 @@ class UDPClient {
         DatagramSocket client_socket = new DatagramSocket();
         InetAddress server_ip = InetAddress.getByName("localhost");
         int server_port = 9090;
-        String http_request = "GET TestFile.html HTTP/1.0 "
+        String http_request = "GET TestFile.html HTTP/1.0";
 
         // Send packet to server
         byte[] data_out = http_request.getBytes();
         DatagramPacket packet_out = new DatagramPacket(
             data_out, data_out.length, server_ip, server_port
         );    
-        clientSocket.send(packet_out);
+        client_socket.send(packet_out);
 
         // receive packet from server
         byte[] data_in = new byte[256];
         DatagramPacket packet_in = new DatagramPacket(data_in, data_in.length);
-        client_socket.receive(packet_in);
+        while (true) {
+            packet_in.setData(data_in);
+            client_socket.receive(packet_in);
+            
+            System.out.println(new String(packet_in.getData()));
+
+            if (new String(packet_in.getData()) == "\0") {
+                System.out.println("fuck");
+                break;
+            }
+        }
 
         client_socket.close();
 
     }
 
-    private static void gremlin(String probability, Packet packet) {
-        Random rand = new Random();
-        double damageProb = rand.nextDouble();
-        double flipProb = rand.nextDouble();
-        int bytesToChange;
+    // private static void gremlin(String probability, Packet packet) {
+    //     Random rand = new Random();
+    //     double damageProb = rand.nextDouble();
+    //     double flipProb = rand.nextDouble();
+    //     int bytesToChange;
 
-        if (flipProb <= 0.5) {
-            bytesToChange = 1;
-        } else if (flipProb <= 0.8) {
-            bytesToChange = 2;
-        } else {
-            bytesToChange = 3;
-        }
+    //     if (flipProb <= 0.5) {
+    //         bytesToChange = 1;
+    //     } else if (flipProb <= 0.8) {
+    //         bytesToChange = 2;
+    //     } else {
+    //         bytesToChange = 3;
+    //     }
 
-        if (damageProb <= Double.parseDouble(probability)) {
-            for (int i = 0; i < bytesToChange; i++) {
-                byte[] data = packet.GETPacketData();
-                int byteNum = rand.nextInt(packet.getPacketDataSize());
-                data[byteNum] = (byte) ~data[byteNum];
-            }
-        }
-    }
+    //     if (damageProb <= Double.parseDouble(probability)) {
+    //         for (int i = 0; i < bytesToChange; i++) {
+    //             byte[] data = packet.GETPacketData();
+    //             int byteNum = rand.nextInt(packet.getPacketDataSize());
+    //             data[byteNum] = (byte) ~data[byteNum];
+    //         }
+    //     }
+    // }
 
-    private static void errorDetection(ArrayList<Packet> packetList) {
-        for (Packet packet : packetList) {
-            Short checkSum = Short.parseShort(packet.getHeaderValue(Packet.HEADER_ELEMENTS.CHECKSUM));
-            byte[] data = packet.GETPacketData();
-            short calculatedCheckSum = Packet.checkSum(data);
-            if (!checkSum.equals(calculatedCheckSum)) {
-                System.out.println("Error detected in Packet Number: " + packet.getHeaderValue(Packet.HEADER_ELEMENTS.SEGMENT_NUMBER));
-            }
-        }
-    }
+    // private static void errorDetection(ArrayList<Packet> packetList) {
+    //     for (Packet packet : packetList) {
+    //         Short checkSum = Short.parseShort(packet.getHeaderValue(Packet.HEADER_ELEMENTS.CHECKSUM));
+    //         byte[] data = packet.GETPacketData();
+    //         short calculatedCheckSum = Packet.checkSum(data);
+    //         if (!checkSum.equals(calculatedCheckSum)) {
+    //             System.out.println("Error detected in Packet Number: " + packet.getHeaderValue(Packet.HEADER_ELEMENTS.SEGMENT_NUMBER));
+    //         }
+    //     }
+    // }
 
 }
--- projects/project_1/UDPServer.java
+++ projects/project_1/UDPServer.java
@@ -1,6 +1,5 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
 
 class UDPServer {   
@@ -12,25 +11,46 @@ class UDPServer {
         // Create byte arrays
         byte[] data_in = new byte[256];
         byte[] data_out  = new byte[256];
+        byte[] data_terminate = "\0".getBytes();
 
-        // Create a scanner to 
+        // Declare a FileInputStream object to convert the requested file to bytes
+        FileInputStream file_bytes;
 
         while(true) {
 
             // Get packet sent from client
             DatagramPacket packet_in = new DatagramPacket(data_in, data_in.length);
             server_socket.receive(packet_in);
-            String sentence = new String(packet_in.getData());
 
-            // Get client IP address and port number
+            // Get client IP address, port number, and HTTP request
             InetAddress client_ip = packet_in.getAddress();
             int client_port = packet_in.getPort();
+            String client_get_request = new String(packet_in.getData());
+
+            // Create the DatagramPacket to send data back to the client
+            DatagramPacket packet_out = new DatagramPacket(
+                data_out, data_out.length, client_ip, client_port
+            );
+
+            // Get the name of the file requested by the client
+            String file_name = client_get_request.substring(
+                client_get_request.indexOf(" ") + 1, client_get_request.lastIndexOf(" ")
+            );
+
+            // Define the FileInputStream object
+            file_bytes = new FileInputStream(new File(file_name));
 
             // Send the requested data to the client
-            DatagramPacket packet_out = new DatagramPacket(
-                packet_out, packet_out.length, client_ip, client_port
-            );
+            while(file_bytes.read(data_out) != -1) {
+                packet_out.setData(data_out);
+                server_socket.send(packet_out);
+            }
+
+            // Notify the client that all data has been sent via a null character
+            packet_out.setData(data_terminate);
             server_socket.send(packet_out);
         }
+
     }
+
 }
