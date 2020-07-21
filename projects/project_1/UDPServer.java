be9326f7aa4898e9cac14a94b215e0a01c041841
AustinBush32 <bush.austin13@gmail.com>
2020-07-21 16:49:21 -0500

    got rid of commented out code

--- projects/project_1/UDPServer.java
+++ projects/project_1/UDPServer.java
@@ -12,13 +12,7 @@ class UDPServer {
 
         // Create byte arrays
         byte[] dataIn = new byte[256];
-        byte[] dataOut  = new byte[256];
         String nullByte = "\0";
-        byte[] dataTerminate = "\0".getBytes();
-
-        // Declare a FileInputStream object to convert the requested file to bytes
-        FileInputStream fileBytes;
-
 
         while(true) {
 
@@ -31,18 +25,10 @@ class UDPServer {
             int clientPort = packetIn.getPort();
             String clientGetRequest = new String(packetIn.getData());
 
-            // Create the DatagramPacket to send data back to the client
-            DatagramPacket packetOut = new DatagramPacket(
-                dataOut, dataOut.length, clientIP, clientPort
-            );
-
             // Get the name of the file requested by the client
             String fileName = clientGetRequest.substring(
                 clientGetRequest.indexOf(" ") + 1, clientGetRequest.lastIndexOf(" ")
             );
-
-            // Define the FileInputStream object
-            // fileBytes = new FileInputStream(new File(fileName));
 
             Scanner fileIn = new Scanner(new File(fileName));
             StringBuilder fileDataContents = new StringBuilder();
@@ -51,7 +37,6 @@ class UDPServer {
                 fileDataContents.append(fileIn.nextLine());
             }
 
-            // System.out.println("File: " + fileDataContents);
             fileIn.close();
 
             String httpHeaderForm = "HTTP/1.0 TestFile.html Follows\r\n"
@@ -65,27 +50,14 @@ class UDPServer {
             for(Packet packet : packetList) {
                 DatagramPacket sendPacket = packet.getDatagramPacket(clientIP, clientPort);
                 serverSocket.send(sendPacket);
-
-                // packetOut.setData(packet.getPacketData()); 
-                // serverSocket.send(packetOut);
             }
-
-            // Send the requested data to the client
-            // while(fileBytes.read(dataOut) != -1) {
-            //     packetOut.setData(dataOut);
-            //     serverSocket.send(packetOut);
-            // }
 
             // Notify the client that all data has been sent via a null character
             System.out.println("Sending null character");
             ArrayList<Packet> nullPacket = Packet.segmentation(nullByte.getBytes());
-            // packetOut.setData(dataTerminate);
-            // packetOut.setData(nullPacket.get(0).getPacketData());
             DatagramPacket nullDatagram = nullPacket.get(0).getDatagramPacket(clientIP, clientPort);
             serverSocket.send(nullDatagram);
             System.out.print("Sent");
-            // serverSocket.close();
-
         }
 
     }
